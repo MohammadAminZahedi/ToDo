20c64cd2828398034b67971d6772375530e70e5e
Amin <amin.zahedi.2002@gmail.com>
2025-08-09 21:08:08 +0330

    Create Domain

---
+++ ToDoList.Domain/Entities/Task.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoList.Domain.Entities
+{
+    public class Task:Base
+    {
+        public string TaskId { get; set; }
+        public string TaskTitle { get; set; }
+        public string TaskDescription { get; set; }
+        public int TaskPriority { get; set; }
+        public string State { get; set; }
+        public bool IsRemoved { get; set; }
+    }
+}
